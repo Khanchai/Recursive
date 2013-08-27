9fa75feb88a4de8d176e47ec2780691998ad16ab
khanchai <khanchai.c@ssd.co.th>
2013-08-27 12:30:30 +0700

    add fibonacci code and testcode

---
+++ Fibonacci/Fibonacci.Tests/FibonacciTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Fibonacci.Tests
+{
+    [TestFixture]
+    public class FibonacciTest
+    {
+        [Test]
+        public void CreateTheFibonacciNumbers()
+        {
+            Assert.That(Fib.CreateNumbers().Take(6), Is.EqualTo(new []{ 1, 1, 2, 3, 5, 8 }));
+        }
+    }
+}
--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fibonacci
 {
+    public class Fib
+    {
+        static int CreateNumbersAtIndex(int index)
+        {
+            if (index < 2) return 1;
+
+            return CreateNumbersAtIndex(index - 1) + CreateNumbersAtIndex(index - 2);
+        }
+
+
+        public static IEnumerable<int> CreateNumbers()
+        {
+            int index = 0;
+            while (true)
+            {
+                yield return CreateNumbersAtIndex(index);
+
+                index++;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
